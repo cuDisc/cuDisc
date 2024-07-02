4cc08e381312263ff897b18fde53f07955f93528
Richard Booth <richardabooth@gmail.com>
2024-07-02 10:48:43 +0100

    Fixed u->0 drag limit

--- headers/drag_const.h
+++ headers/drag_const.h
@@ -6,7 +6,7 @@
 #include "dustdynamics1D.h"
 
 __device__ __host__
-inline double calc_C_D(double a, double rho_g, double cs, double u_rel, double mu) {
+inline double calc_vC_D(double a, double rho_g, double cs, double u_rel, double mu) {
 
     // https://www.aanda.org/articles/aa/pdf/2018/03/aa31824-17.pdf
 
@@ -21,55 +21,69 @@ inline double calc_C_D(double a, double rho_g, double cs, double u_rel, double m
     double G = pow(10.,2.5*pow(Re/312.,0.6688) / (1.+pow(Re/312.,0.6688)));
     double H = 4.6/(1.+M) + 1.7;
 
-    double C_inc = 24./Re * (1.+0.15*pow(Re,0.687)) + 0.407*Re / (Re+8710);
-    double C_D = 2.+(C_inc-2.)*exp(-(3.07*sqrtgamma*M*G)/Re) + H * exp(-Re/(2*M)) / (sqrtgamma*M);
+    double vC_inc = 12 * nu_mol / a * (1.+0.15*pow(Re,0.687)) + 0.407*u_rel * Re / (Re+8710);
+    double vC_D = 2 * u_rel + (vC_inc-2*u_rel)*exp(-1.535*sqrtgamma*G*nu_mol/(a*cs)) + H * exp(-a*cs/nu_mol) * cs / sqrtgamma;
     
-    return C_D;
+    return vC_D;
 }
 
 __device__ __host__
-inline double calc_C_D_step(double a, double rho_g, double cs, double u_rel, double mu, double mfp) {
+inline double calc_vC_D_step(double a, double rho_g, double cs, double u_rel, double mu, double mfp) {
 
     double nu_mol = 1.595769122 * 0.5 * mfp*cs;
     double Re = (2.*a*u_rel)/nu_mol;
 
     if (Re < 1.) {
-        return 24./Re;
+        return 12 * nu_mol / a ;
     }
     else if (Re < 800.) {
-        return 24./pow(Re,0.6);
+        return 24. * u_rel / pow(Re,0.6);
     }
     else {
-        return 0.44;
+        return 0.44 * u_rel ;
     }
 }
 
 __device__ __host__
+template<bool use_full_stokes=false>
 inline double calc_t_s(Prims wd, Prims wg, double a, double rho_m, double cs, double mu) {
 
-    double mfp = mu*m_H/(wg.rho*2e-15);
-
-    if (a<2.25*mfp) 
-        return 0.6266570686577501 * rho_m * a / (wg.rho*cs) ;
-    else {
+    if (use_full_stokes) {
         double u_rel = sqrt((wd.v_phi-wg.v_phi)*(wd.v_phi-wg.v_phi) 
-                            + (wd.v_R-wg.v_R)*(wd.v_R-wg.v_R) + (wd.v_Z-wg.v_Z)*(wd.v_Z-wg.v_Z));
-        return 2.6666666666666667 * rho_m * a / (wg.rho * calc_C_D_step(a,wg.rho,cs,u_rel,mu,mfp) * u_rel);
+                                + (wd.v_R-wg.v_R)*(wd.v_R-wg.v_R) + (wd.v_Z-wg.v_Z)*(wd.v_Z-wg.v_Z));
+        return 2.6666666666666667 * rho_m * a / (wg.rho * calc_vC_D(a,wg.rho,cs,u_rel,mu));
+    } else {
+        double mfp = mu*m_H/(wg.rho*2e-15);
+        
+        if (a<2.25*mfp) 
+            return 0.6266570686577501 * rho_m * a / (wg.rho*cs) ;
+        else {
+            double u_rel = sqrt((wd.v_phi-wg.v_phi)*(wd.v_phi-wg.v_phi) 
+                                + (wd.v_R-wg.v_R)*(wd.v_R-wg.v_R) + (wd.v_Z-wg.v_Z)*(wd.v_Z-wg.v_Z));
+            return 2.6666666666666667 * rho_m * a / (wg.rho * calc_vC_D_step(a,wg.rho,cs,u_rel,mu,mfp));
+        }
     }
 }
 
 __device__ __host__
+template<bool use_full_stokes=false>
 inline double calc_t_s(Prims1D wd, Prims1D wg, double a, double rho_m, double cs, double mu, double Om) {
 
     double rho_g = wg.Sig/(2.506628275 * cs/Om);
-    double mfp = mu*m_H/(rho_g*2e-15);
 
-    if (a<2.25*mfp) {
-        return 0.6266570686577501 * rho_m * a / (rho_g*cs) ;
-    }
-    else {
+    if (use_full_stokes) {
         double u_rel = sqrt((wd.v_R-wg.v_R)*(wd.v_R-wg.v_R) + wd.dv_phi*wd.dv_phi + wd.dv_Z*wd.dv_Z);
-        return 2.6666666666666667 * rho_m*a/ (rho_g * calc_C_D_step(a,rho_g,cs,u_rel,mu,mfp) * u_rel);
+        return 2.6666666666666667 * rho_m*a/ (rho_g * calc_vC_D(a,rho_g,cs,u_rel,mu));
+    } else {
+        double mfp = mu*m_H/(rho_g*2e-15);
+
+        if (a<2.25*mfp) {
+            return 0.6266570686577501 * rho_m * a / (rho_g*cs) ;
+        }
+        else {
+            double u_rel = sqrt((wd.v_R-wg.v_R)*(wd.v_R-wg.v_R) + wd.dv_phi*wd.dv_phi + wd.dv_Z*wd.dv_Z);
+            return 2.6666666666666667 * rho_m*a/ (rho_g * calc_vC_D_step(a,rho_g,cs,u_rel,mu,mfp));
+        }
     }
 }
 
