f25f99d76b5988a4e475c2e0b20897e2da00b6f2
Alfie Robinson <a.robinson21@imperial.ac.uk>
2024-07-03 10:36:52 +0100

    tweaks to 1D disc run

--- codes/1Ddisc_multgrain.cpp
+++ codes/1Ddisc_multgrain.cpp
@@ -31,6 +31,7 @@ void set_up(Grid& g, Field3D<Prims1D>& W_d, Field<Prims1D>& W_g, SizeGrid& sizes
         cs(i,g.Nghost) = std::sqrt(k_B*T(i,g.Nghost)/(2.4*m_H));
 
         W_g(i,g.Nghost).v_R = 0;
+        W_g(i,g.Nghost).v_phi = v_k;
         
         nu[i] = alpha * cs(i,g.Nghost)*cs(i,g.Nghost) * (g.Rc(i)/v_k);
         alpha2D(i,g.Nghost) = alpha;
@@ -49,6 +50,7 @@ void set_up(Grid& g, Field3D<Prims1D>& W_d, Field<Prims1D>& W_g, SizeGrid& sizes
             Sig_tot += W_d(i,g.Nghost,k).Sig;
             Mdtot += 2.*M_PI*g.Rc(i)*W_d(i,g.Nghost,k).Sig*g.dRe(i);
             W_d(i,g.Nghost,k).v_R = W_g(i,g.Nghost).v_R;
+            W_d(i,g.Nghost,k).v_phi = W_g(i,g.Nghost).v_phi;
             D(i,g.Nghost,k) =  W_g(i,g.Nghost).Sig * nu[i]/Sc;
         }
         for (int k=0; k<W_d.Nd; k++) {
@@ -106,13 +108,13 @@ int main() {
 
     set_up(g, W_d, W_g, sizes, T, cs, nu, alpha2D, D, alpha, star);
 
-    DustDyn1D dyn(D, cs, star, sizes, nu, 2.4, alpha, 0.4, 0.1, floor, gas_floor);
+    DustDyn1D dyn(D, cs, star, sizes, 2.4, alpha, 0.4, 0.1, floor, gas_floor);
     int boundary = BoundaryFlags::open_R_inner | BoundaryFlags::open_R_outer;
     dyn.set_boundaries(boundary);
 
     BirnstielKernelVertInt kernel = BirnstielKernelVertInt(g, sizes, W_d, W_g, cs, alpha2D, 2.4, M_star);
     kernel.set_fragmentation_threshold(v_frag);
-    BS32Integration<CoagulationRate<BirnstielKernelVertInt, SimpleErosion>>
+    BS32Integration<CoagulationRate<decltype(kernel), SimpleErosion>>
         coagulation_integrate(
             create_coagulation_rate(
                 sizes, 
@@ -137,7 +139,7 @@ int main() {
 
     int Nout = 1;
 
-    calc_v_gas(g, W_g, nu, star.GM, gas_floor);
+    calc_v_gas(g, W_g, cs, nu, star.GM, gas_floor);
     write_prims1D(dir, 0, g, W_d, W_g);
 
     for (double ti : ts) {
@@ -160,7 +162,7 @@ int main() {
             dyn(g, W_d, W_g, dt);
 
             update_gas_sigma(g, W_g, dt, nu, gas_boundary, gas_floor);
-            calc_v_gas(g, W_g, nu, star.GM, gas_floor);
+            calc_v_gas(g, W_g, cs, nu, star.GM, gas_floor);
 
             if ((t+dt >= t_coag+dt_coag)|| (t+2*dt >= t_coag+dt_coag && dt < dt_coag) || dt == ti-t) {
                 std::cout << "Coag step at count = " << count << "\n";
