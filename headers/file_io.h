a49723822489fb7529efd36d69e68e47b19d4c2c
Alfie Robinson <a.robinson21@imperial.ac.uk>
2024-11-15 10:29:57 +0000

    included 1D velocity data in file for restart

--- headers/file_io.h
+++ headers/file_io.h
@@ -154,11 +154,11 @@ void write_prims1D(std::filesystem::path folder, out_type out, Grid &g, Field3D<
     f.write((char*) &NR, sizeof(int));
     f.write((char*) &nspec, sizeof(int));
     for (int i=0; i<g.NR+2*g.Nghost; i++) {
-        for (int k=0; k<2; k++) {
+        for (int k=0; k<4; k++) {
             f.write((char*) &wg(i,g.Nghost)[k], sizeof(double));
         }
         for (int k=0; k<wd.Nd; k++) {
-            for (int l=0; l<2; l++) {
+            for (int l=0; l<4; l++) {
                 f.write((char*) &wd(i,g.Nghost,k)[l], sizeof(double));
             }
         }
@@ -230,11 +230,11 @@ void read_prims1D(std::filesystem::path folder, out_type out, Field3D<Prims1D>& 
     f.read((char*) &NR, sizeof(int));
     f.read((char*) &nspec, sizeof(int));
     for (int i=0; i<NR; i++) {
-        for (int k=0; k<2; k++) {
+        for (int k=0; k<4; k++) {
             f.read((char*) &wg(i,2)[k], sizeof(double));
         }
         for (int k=0; k<nspec; k++) {
-            for (int l=0; l<2; l++) {
+            for (int l=0; l<4; l++) {
                 f.read((char*) &wd(i,2,k)[l], sizeof(double));
             }
         }
